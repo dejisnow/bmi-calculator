987ce5a3277f27110911b0c9dd88d39bc323eccf
Deji Snow <109693282+dejisnow@users.noreply.github.com>
2024-06-09 22:35:54 +0100

    Update bmi.js

--- bmi.js
+++ bmi.js
@@ -1,1 +1,15 @@
 
+window.onload=()=>{
+    let weight = document.querySelector(".weightDiv")
+    
+    let height= document.querySelector(".heightDiv")
+    
+    let submit = document.querySelector("sunmit")
+    
+    submit.addEventListener("click",()=>{
+        
+        
+        
+    })
+    
+}
