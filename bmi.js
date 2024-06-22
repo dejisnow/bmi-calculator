b06c184af2e362f39753595383ebbbe3a9310052
Deji Snow <109693282+dejisnow@users.noreply.github.com>
2024-06-22 22:55:07 +0100

    Update bmi.js

--- bmi.js
+++ bmi.js
@@ -1,15 +1,33 @@
-
 window.onload=()=>{
-    let weight = document.querySelector(".weightDiv")
+let ageSelect = document.getElementById("age")
+    let weight = document.querySelector(".weightInput")
     
-    let height= document.querySelector(".heightDiv")
+    let height= document.querySelector(".heightInput")
     
-    let submit = document.querySelector("sunmit")
+    let submit = document.querySelector(".submit")
+        let result = document.querySelector(".result")
+    
+    
     
     submit.addEventListener("click",()=>{
+    
+    function bmi(){
+   console.log(ageSelect.options[selected].text)
+      
+    return  parseInt(weight.value)/parseInt((height.value)**2)
+      }
         
-        
+      result.innerHTML= bmi()
         
     })
+    
+    ageSelect.addEventListener("change",()=>{
+    let selected = ageSelect.selectedIndex;
+    
+       console.log(ageSelect.options[selected].text)
+    })
+    
+    
+    
     
 }
